25d15d1bc179366e5792c5afe647ea56fb9d6a68
Huseyn Huseynov <huseynhuseyn343@gmail.com>
2025-04-10 01:36:32 +0400

    feat(homework): Add example for return and break usage

---
+++ src/src_summary/Lesson_5/ReturnAndBreak.java
@@ -0,0 +1,28 @@
+package src_summary.Lesson_5;
+
+public class ReturnAndBreak {
+    public static void main(String[] args) {
+
+        example();
+
+    }
+
+    public static void example() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 3) {
+                return; // Stop Method
+            }
+            System.out.println("return example: " + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 3) {
+                break; // Stop loop
+            }
+            System.out.println("break example: " + i);
+        }
+
+        System.out.println("This Line can't run after return!");
+    }
+
+}
