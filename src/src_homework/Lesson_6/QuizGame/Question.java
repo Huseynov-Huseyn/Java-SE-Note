eac96308cd4877ca820e1bfd4ecf016f85cacc30
Huseyn Huseynov <huseynhuseyn343@gmail.com>
2025-04-27 02:10:23 +0400

    style(code): clean up extra blank lines and spaces

--- src/src_homework/Lesson_6/QuizGame/Question.java
+++ src/src_homework/Lesson_6/QuizGame/Question.java
@@ -1,50 +1,50 @@
-    package src_homework.Lesson_6.QuizGame;
+package src_homework.Lesson_6.QuizGame;
 
-    public class Question {
+public class Question {
 
-        String questionText;
-        String variantA;
-        String variantB;
-        String variantC;
-        String correctVariant;
+    String questionText;
+    String variantA;
+    String variantB;
+    String variantC;
+    String correctVariant;
 
-        public Question(String questionText, String variantA, String variantB, String variantC, String correctVariant) {
-            this.questionText = questionText;
-            this.variantA = variantA;
-            this.variantB = variantB;
-            this.variantC = variantC;
-            this.correctVariant = correctVariant;
-        }
-
-        public String getQuestionText() {
-            return questionText;
-        }
-
-        public String getCorrectVariant() {
-            return correctVariant;
-        }
-
-        public String getVariantA() {
-            return variantA;
-        }
-
-        public String getVariantB() {
-            return variantB;
-        }
-
-        public String getVariantC() {
-            return variantC;
-        }
-
-        @Override
-        public String toString() {
-            return "Question{" +
-                    "correctVariant='" + correctVariant + '\'' +
-                    ", questionText='" + questionText + '\'' +
-                    ", variantA='" + variantA + '\'' +
-                    ", variantB='" + variantB + '\'' +
-                    ", variantC='" + variantC + '\'' +
-                    '}';
-        }
-
+    public Question(String questionText, String variantA, String variantB, String variantC, String correctVariant) {
+        this.questionText = questionText;
+        this.variantA = variantA;
+        this.variantB = variantB;
+        this.variantC = variantC;
+        this.correctVariant = correctVariant;
     }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getCorrectVariant() {
+        return correctVariant;
+    }
+
+    public String getVariantA() {
+        return variantA;
+    }
+
+    public String getVariantB() {
+        return variantB;
+    }
+
+    public String getVariantC() {
+        return variantC;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "correctVariant='" + correctVariant + '\'' +
+                ", questionText='" + questionText + '\'' +
+                ", variantA='" + variantA + '\'' +
+                ", variantB='" + variantB + '\'' +
+                ", variantC='" + variantC + '\'' +
+                '}';
+    }
+
+}
--- src/src_summary/Lesson_6/Objects/Main.java
+++ src/src_summary/Lesson_6/Objects/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         Car firstCar = new Car();
-        firstCar.brand="BMW";
-        firstCar.model="I5";
-        firstCar.year=2022;
+        firstCar.brand = "BMW";
+        firstCar.model = "I5";
+        firstCar.year = 2022;
 
         System.out.println(firstCar);
     }
